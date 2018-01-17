0459c8dbc3bc0881da4c97254f6d574875e4cb69
Kier Borromeo <seraphipod@gmail.com>
2018-01-18 00:18:59 +0800

    Add react to drag over (multiple) example

--- storybook/uploadi.js
+++ storybook/uploadi.js
@@ -137,3 +137,49 @@ storiesOf('Uploadi', module)
 
     return <Droppable />
   })
+  // This example shows you how you can display
+  // react to multiple uploaded files
+  .add('react to drag over (multiple)', () => {
+    class Multiple extends React.Component {
+      state = {
+        // Here goes the base64 parsed events
+        images: [],
+        // Here goes the original `File`s
+        files: []
+      }
+
+      render() {
+        const {images} = this.state
+
+        return (
+          <Uploadi multiple onFiles={this.handleFiles}>
+            {({over, onSelect}) => {
+              return (
+                <div className={c('upload-box', { '-over': over })}>
+                  {Boolean(images.length) && <div className="attachment-list">
+                    {Boolean(images.length) && images.map((image, i) => {
+                      return <img src={image} className="item" key={i} />
+                    })}
+                  </div>}
+
+                  <div className="footer">
+                    <h1 className="heading">Drop Files</h1>
+                    <p className="sub"><span className="drag">Drag here</span> <span className="or">or</span> <span className="browse" onClick={onSelect}>browse</span> to upload</p>
+                  </div>
+                </div>
+              )
+            }}
+          </Uploadi>
+        )
+      }
+
+      handleFiles = (files, images) => {
+        this.setState({
+          files,
+          images
+        })
+      }
+    }
+
+    return <Multiple />
+  })
