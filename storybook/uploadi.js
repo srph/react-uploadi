75adb606f0aa83be8fcb38effc326277351f3a0e
Kier Borromeo <seraphipod@gmail.com>
2018-02-12 13:18:09 +0800

    Add example for loading states

--- storybook/uploadi.js
+++ storybook/uploadi.js
@@ -137,10 +137,11 @@ storiesOf('Uploadi', module)
 
     return <Droppable />
   })
+
   // This example shows you how you can display
   // react to multiple uploaded files
   .add('react to drag over (multiple)', () => {
-    class Multiple extends React.Component {
+    class MultipleDragOver extends React.Component {
       state = {
         // Here goes the base64 parsed events
         images: [],
@@ -181,5 +182,97 @@ storiesOf('Uploadi', module)
       }
     }
 
-    return <Multiple />
+    return <MultipleDragOver />
   })
+
+  // This example shows you how you can add a loading state.
+  // @see https://medium.com/@srph/react-maintaining-state-for-collections-80a1d9615886
+  .add('loading state for multiple uploads', () => {
+    // We'll have a simple counter here
+    // to assign ids for each selected file
+    let id = 0
+
+    class MultipleLoadingState extends React.Component {
+      state = {
+        // Here goes the base64 parsed events
+        // [{ id: 1, url: string }]
+        images: [],
+        // Here goes the original `File`s
+        files: [],
+        // Here, we have an object that contains the ID of our images.
+        // { '5': 0, '6': 32 }
+        loading: {}
+      }
+
+      render() {
+        const {images} = this.state
+
+        return (
+          <Uploadi multiple onFiles={this.handleFiles}>
+            {({over, onSelect}) => {
+              return (
+                <div className={c('upload-box', { '-over': over })}>
+                  {Boolean(images.length) && <div className="attachment-list">
+                    {Boolean(images.length) && images.map((image, i) => {
+                      return <img src={image.url} className={c('item', {
+                        '-loading': this.state.loading[image.id]
+                      })} key={i} />
+                    })}
+                  </div>}
+
+                  <div className="footer">
+                    <h1 className="heading">Drop Files</h1>
+                    <p className="sub"><span className="drag">Drag here</span> <span className="or">or</span> <span className="browse" onClick={onSelect}>browse</span> to upload</p>
+                  </div>
+                </div>
+              )
+            }}
+          </Uploadi>
+        )
+      }
+
+      handleFiles = (files, images) => {
+        // [string, string, string] ->
+        // [{ id: 1, url: string }, { id: 2, url: string }, { id: 3, url: string }]
+        images = images.map((image, i) => {
+          return { id: id++, url: image }
+        })
+
+        // Assign ids to the loading state
+        // { '1': true, '2': true, '3': true }
+        const loading = {}
+        images.forEach(image => {
+          loading[image.id] = true
+        })
+
+        this.setState({
+          files: [...this.state.files, ...files],
+          images: [...this.state.images, ...images],
+          loading: { ...this.state.loading, ...loading }
+        })
+
+        // We'll have a "fake ajax" here.
+        images.forEach(image => {
+          ajax().then(() => {
+            const loading = { ...this.state.loading }
+            delete loading[image.id]
+            this.setState({ loading })
+          })
+        })
+      }
+    }
+
+    return <MultipleLoadingState />
+  })
+
+
+// Fake "ajax" that resolves between 350ms to 1s
+function ajax() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, random(500, 2000))
+  })
+}
+
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
