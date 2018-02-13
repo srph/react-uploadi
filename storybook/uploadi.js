52c68e1e1d27c7eebc2f80f21edffc17284030d9
Kier Borromeo <seraphipod@gmail.com>
2018-02-13 20:47:56 +0800

    Add actual file upload

--- storybook/uploadi.js
+++ storybook/uploadi.js
@@ -273,10 +273,80 @@ storiesOf('Uploadi', module)
     return <MultipleLoadingState />
   })
 
+  // This example shows you how you can do a full-blown
+  // file upload. Shows a loading state.
+  // Inspect the XHR logs in your browser's DevTools.
+  // Thanks to file.io!
+  .add('actual file upload', () => {
+    class FullBlownFileUpload extends React.Component {
+      state = {
+        // Here goes the base64 parsed event
+        image: '',
+        // Here goes the original File
+        file: null,
+        // ...
+        loading: false
+      }
+
+      render() {
+        const {image} = this.state
+
+        return (
+          <Uploadi onFiles={this.handleFiles}>
+            {({over, onSelect}) => {
+              return (
+                <div className={c('avatar-box', {
+                    '-default': !over,
+                    '-over': over,
+                    '-loading': this.state.loading
+                  })}>
+                  <img src={image || defaultAvatar} className="superpogi" />
+                  {!over && <div className="overlay">
+                    <button className="button" onClick={onSelect}>
+                      Browse
+                    </button>
+                  </div>}
+                  {over && <div className="overlay2">
+                    <div className="dashed" />
+                  </div>}
+                </div>
+              )
+            }}
+          </Uploadi>
+        )
+      }
+
+      handleFiles = (file, image) => {
+        this.setState({
+          file,
+          image
+        })
+
+        const payload = new FormData()
+        payload.append('file', file)
+
+        axios.post('https://file.io', payload).then(res => {
+          this.setState({
+            loading: false
+          })
+        }).catch(err => {
+          this.setState({
+            file: null,
+            image: '',
+            loading: false
+          })
+        })
+      }
+    }
+
+    return <FullBlownFileUpload />
+  })
+
   // This example shows you how you can do a full-blown file-upload
   // with axios. Also adds a loading, progress, error, and success state.
+  // Inspect the XHR logs in your browser's DevTools.
   // Thanks, file.io!
-  .add('actual file upload', () => {
+  .add('actual file upload (multiple)', () => {
     // We'll have a simple counter here
     // to assign ids for each selected file
     let id = 0
